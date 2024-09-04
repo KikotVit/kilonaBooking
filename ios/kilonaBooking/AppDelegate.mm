631459b6925a26b82869bbfadad1198f89793019
Kikot Vitalii <vitalii.kikot@softengi.com>
2024-09-04 22:54:58 +0200

    add maps & permissions libs

--- ios/kilonaBooking/AppDelegate.mm
+++ ios/kilonaBooking/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -9,6 +10,7 @@
   self.moduleName = @"kilonaBooking";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
+  [GMSServices provideAPIKey:@"AIzaSyCEaOoNWKrl4QBjOs5RSk7CNjlaW0di-0I"]; // add this line using the api key obtained from Google Console
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- src/screens/map/map.screen.tsx
+++ src/screens/map/map.screen.tsx
@@ -1,14 +1,68 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Screen } from '../../components';
+import React, { forwardRef, useRef } from 'react';
+import { Text, View, ViewStyle } from 'react-native';
+import { RouteInfoHeader, Screen } from '../../components';
+import MapView, { MapViewProps, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useRouteContext } from '../../context/route.context';
+import { SCREEN_PADDING } from '../../theme';
+
+export const INITIAL_REGION = {
+    latitude: 48,
+    longitude: 31,
+    latitudeDelta: 17,
+    longitudeDelta: 17,
+};
+
+const Map = ((props: MapViewProps, ref) => (
+    <View
+        style={{
+            width: '100%',
+            height: '100%',
+        }}
+    >
+        <MapView
+            ref={ref} 
+            region={INITIAL_REGION}
+            provider={PROVIDER_GOOGLE}
+            style={MAP_VIEW}
+            showsUserLocation
+            showsMyLocationButton={true}
+            toolbarEnabled={false} 
+            {...props} />
+    </View>
+));
+
+const MapComponent = React.memo(React.forwardRef(Map));
 
 export const MapScreen = () => {
+    const { route } = useRouteContext();
+    const mapRef = useRef();
+
     return (
         <Screen
         >
-            <Text>
-    MAP
-            </Text>
+            <View
+                style={ROOT}
+            >
+                <RouteInfoHeader
+                    routeName={route.routeName}
+                    routeConnection={route.routeConnection}
+                />
+                <MapComponent
+                    ref={mapRef}
+                />
+            </View>
         </Screen>
     );
+};
+
+const ROOT: ViewStyle = {
+    alignSelf: 'center',
+    width: '100%',
+    rowGap: SCREEN_PADDING,
+    alignItems: 'flex-start',
+};
+
+const MAP_VIEW: ViewStyle = {
+    width: '100%',
+    flex: 1,
 };
